9155e87aeba317fcdd3035a9545afc809461fc7b
Ömer Algin <oemer-algin@hotmail.de>
2022-12-23 20:46:50 +0100

    complete navbar with react-router and hash-link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <NavBar />
     </div>
   );
 }
